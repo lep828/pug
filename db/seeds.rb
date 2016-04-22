281c7a0d29eee5f9e29a69c83c9d8d24c2c0bcf2
lep828 <lep828@gmail.com>
2016-04-22 17:06:26 +0100

    Working on trying to add games to groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.destroy_all
 Group.destroy_all
+Game.destroy_all
 Subscriber.destroy_all
 
 u1 = User.create!(username: "peter", email: "peter@peter.com", image: "https://www.fillmurray.com/500/500", description: "le epic gamer", password: "password")
@@ -8,7 +9,10 @@ u2 = User.create!(username: "peter2", email: "peter@peter2.com", image: "https:/
 
 u3 = User.create!(username: "peter3", email: "peter@peter3.com", image: "https://www.fillmurray.com/350/350", description: "goat3", password: "password")
 
-g1 = Group.create!(name: "hey", description: "we the greatest.", image: "https://www.fillmurray.com/300/300", creator_id: u1.id)
+g1 = Group.create!(name: "heythere", description: "we the greatest.", image: "https://www.fillmurray.com/300/300", creator_id: u1.id)
 
-Subscriber.create!(group_id: g1.id, user_id: u2.id)
-Subscriber.create!(group_id: g1.id, user_id: u3.id)
+Subscriber.create!(group_id: g1.id, user_id: u1.id, admin: true)
+Subscriber.create!(group_id: g1.id, user_id: u2.id, admin: false)
+Subscriber.create!(group_id: g1.id, user_id: u3.id, admin: false)
+
+game1 = Game.create!(name: "game1", description: "askdflasmdfk", image:"https://www.fillmurray.com/200/200")
